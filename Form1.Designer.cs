4ed6a2fb361d603772de5340b25131c10e0f5614
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2024-11-23 22:23:51 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewWindows = new System.Windows.Forms.DataGridView();
+            this.saveCurrentLayout = new System.Windows.Forms.Button();
+            this.loadLastLayout = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.splitSidewaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitHorizontallyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadLayoutFromFileButton = new System.Windows.Forms.Button();
+            this.saveLayoutAsFileButton = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Button = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Left = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Top = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Right = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bottom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.minimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWindows)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewWindows
+            // 
+            this.dataGridViewWindows.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewWindows.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewWindows.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Button,
+            this.Left,
+            this.Top,
+            this.Right,
+            this.Bottom});
+            this.dataGridViewWindows.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewWindows.Name = "dataGridViewWindows";
+            this.dataGridViewWindows.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewWindows.Size = new System.Drawing.Size(776, 329);
+            this.dataGridViewWindows.TabIndex = 0;
+            this.dataGridViewWindows.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewWindows_CellContentClick);
+            this.dataGridViewWindows.Click += new System.EventHandler(this.dataGridViewWindows_Click);
+            // 
+            // saveCurrentLayout
+            // 
+            this.saveCurrentLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveCurrentLayout.Location = new System.Drawing.Point(12, 347);
+            this.saveCurrentLayout.Name = "saveCurrentLayout";
+            this.saveCurrentLayout.Size = new System.Drawing.Size(121, 23);
+            this.saveCurrentLayout.TabIndex = 1;
+            this.saveCurrentLayout.Text = "Save Current Layout";
+            this.saveCurrentLayout.UseVisualStyleBackColor = true;
+            this.saveCurrentLayout.Click += new System.EventHandler(this.saveCurrentLayout_Click);
+            // 
+            // loadLastLayout
+            // 
+            this.loadLastLayout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.loadLastLayout.Location = new System.Drawing.Point(12, 376);
+            this.loadLastLayout.Name = "loadLastLayout";
+            this.loadLastLayout.Size = new System.Drawing.Size(121, 23);
+            this.loadLastLayout.TabIndex = 2;
+            this.loadLastLayout.Text = "Load Last Layout";
+            this.loadLastLayout.UseVisualStyleBackColor = true;
+            this.loadLastLayout.Click += new System.EventHandler(this.loadLastLayout_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.splitSidewaysToolStripMenuItem,
+            this.splitHorizontallyToolStripMenuItem,
+            this.minimizeToolStripMenuItem,
+            this.restoreToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(165, 92);
+            // 
+            // splitSidewaysToolStripMenuItem
+            // 
+            this.splitSidewaysToolStripMenuItem.Name = "splitSidewaysToolStripMenuItem";
+            this.splitSidewaysToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.splitSidewaysToolStripMenuItem.Text = "Split Sideways";
+            this.splitSidewaysToolStripMenuItem.Click += new System.EventHandler(this.splitSidewaysToolStripMenuItem_Click);
+            // 
+            // splitHorizontallyToolStripMenuItem
+            // 
+            this.splitHorizontallyToolStripMenuItem.Name = "splitHorizontallyToolStripMenuItem";
+            this.splitHorizontallyToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.splitHorizontallyToolStripMenuItem.Text = "Split Horizontally";
+            this.splitHorizontallyToolStripMenuItem.Click += new System.EventHandler(this.splitHorizontallyToolStripMenuItem_Click);
+            // 
+            // loadLayoutFromFileButton
+            // 
+            this.loadLayoutFromFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.loadLayoutFromFileButton.Location = new System.Drawing.Point(213, 376);
+            this.loadLayoutFromFileButton.Name = "loadLayoutFromFileButton";
+            this.loadLayoutFromFileButton.Size = new System.Drawing.Size(121, 23);
+            this.loadLayoutFromFileButton.TabIndex = 4;
+            this.loadLayoutFromFileButton.Text = "Load Layout from File";
+            this.loadLayoutFromFileButton.UseVisualStyleBackColor = true;
+            this.loadLayoutFromFileButton.Click += new System.EventHandler(this.loadLayoutFromFileButton_Click);
+            // 
+            // saveLayoutAsFileButton
+            // 
+            this.saveLayoutAsFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveLayoutAsFileButton.Location = new System.Drawing.Point(213, 347);
+            this.saveLayoutAsFileButton.Name = "saveLayoutAsFileButton";
+            this.saveLayoutAsFileButton.Size = new System.Drawing.Size(121, 23);
+            this.saveLayoutAsFileButton.TabIndex = 3;
+            this.saveLayoutAsFileButton.Text = "Save Layout as File";
+            this.saveLayoutAsFileButton.UseVisualStyleBackColor = true;
+            this.saveLayoutAsFileButton.Click += new System.EventHandler(this.saveLayoutAsFileButton_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Window Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Handle";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Button
+            // 
+            this.Button.HeaderText = "Button";
+            this.Button.Name = "Button";
+            this.Button.ReadOnly = true;
+            this.Button.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // Left
+            // 
+            this.Left.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Left.HeaderText = "Left";
+            this.Left.Name = "Left";
+            this.Left.Width = 50;
+            // 
+            // Top
+            // 
+            this.Top.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Top.HeaderText = "Top";
+            this.Top.Name = "Top";
+            this.Top.Width = 51;
+            // 
+            // Right
+            // 
+            this.Right.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Right.HeaderText = "Right";
+            this.Right.Name = "Right";
+            this.Right.Width = 57;
+            // 
+            // Bottom
+            // 
+            this.Bottom.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Bottom.HeaderText = "Bottom";
+            this.Bottom.Name = "Bottom";
+            this.Bottom.Width = 65;
+            // 
+            // minimizeToolStripMenuItem
+            // 
+            this.minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+            this.minimizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minimizeToolStripMenuItem.Text = "Minimize";
+            this.minimizeToolStripMenuItem.Click += new System.EventHandler(this.minimizeToolStripMenuItem_Click);
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restoreToolStripMenuItem.Text = "Restore";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadLayoutFromFileButton);
+            this.Controls.Add(this.saveLayoutAsFileButton);
+            this.Controls.Add(this.loadLastLayout);
+            this.Controls.Add(this.saveCurrentLayout);
+            this.Controls.Add(this.dataGridViewWindows);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewWindows)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewWindows;
+        private System.Windows.Forms.Button saveCurrentLayout;
+        private System.Windows.Forms.Button loadLastLayout;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem splitSidewaysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem splitHorizontallyToolStripMenuItem;
+        private System.Windows.Forms.Button loadLayoutFromFileButton;
+        private System.Windows.Forms.Button saveLayoutAsFileButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewButtonColumn Button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Left;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Top;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Right;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bottom;
+        private System.Windows.Forms.ToolStripMenuItem minimizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,298 @@
+﻿using Polenter.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowMover
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern int GetProcessId(IntPtr handle);
+        public delegate bool EnumChildCallback(IntPtr hwnd, ref IntPtr lParam);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool EnumChildWindows(IntPtr hWndParent, EnumChildCallback lpEnumFunc, ref IntPtr lParam);
+
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(HandleRef hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int width, int height, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, uint Msg);
+
+        private const uint SW_RESTORE = 0x09;
+        private const uint SW_MINIMIZE = 0x06;
+        private const uint SW_NORMAL = 0x01;
+        const short SWP_NOMOVE = 0X2;
+        const short SWP_NOSIZE = 1;
+        const short SWP_NOZORDER = 0X4;
+        const int SWP_SHOWWINDOW = 0x0040;
+        public Form1()
+        {
+            InitializeComponent();
+            GetAllWindows();
+        }
+
+        public void MoveSpecificWindow(IntPtr handle, int X, int Y, int width, int height)
+        {
+            ShowWindow(handle, SW_RESTORE);
+            SetWindowPos(handle, 0, X, Y, width, height, SWP_NOZORDER | SWP_SHOWWINDOW);
+
+        }
+
+        public void GetAllWindows()
+        {
+            dataGridViewWindows.Rows.Clear();
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                AllTheOtherStuff atos = new AllTheOtherStuff();
+                IDictionary<IntPtr, string> windows = atos.GetOpenWindowsFromPID(process.Id);
+                foreach (KeyValuePair<IntPtr, string> kvp in windows)
+                {
+                    RECT windowRectangle;
+                    new HandleRef();
+                    GetWindowRect(new HandleRef(process, process.MainWindowHandle), out windowRectangle);
+                    dataGridViewWindows.Rows.Add(kvp.Value, kvp.Key.ToInt32(), "Reset", windowRectangle.Left, windowRectangle.Top, windowRectangle.Right, windowRectangle.Bottom);
+                }
+
+                /*
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+
+
+
+
+                    RECT windowRectangle;
+                    new HandleRef();
+                    GetWindowRect(new HandleRef(process, process.MainWindowHandle), out windowRectangle);
+                    dataGridViewWindows.Rows.Add(process.MainWindowTitle, process.MainWindowHandle.ToInt32(), "Reset", windowRectangle.Left, windowRectangle.Top, windowRectangle.Right, windowRectangle.Bottom);
+
+
+
+                    //Console.WriteLine("Process: {0} ID: {1} Window title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
+                }*/
+            }
+        }
+
+        public void ResetWindows()
+        {
+            for (int i = 0; i < dataGridViewWindows.Rows.Count; i++)
+            {
+
+                try
+                {
+                    if (dataGridViewWindows.Rows[i]?.Cells[1]?.Value != null)
+                    {
+
+                        var handle = new IntPtr((int)dataGridViewWindows.Rows[i]?.Cells[1]?.Value);
+                        RECT windowRectangle = new RECT();
+                        windowRectangle.Left = (int)dataGridViewWindows.Rows[i].Cells["Left"]?.Value;
+                        windowRectangle.Top = (int)dataGridViewWindows.Rows[i].Cells["Top"]?.Value;
+                        windowRectangle.Right = (int)dataGridViewWindows.Rows[i].Cells["Right"]?.Value;
+                        windowRectangle.Bottom = (int)dataGridViewWindows.Rows[i].Cells["Bottom"]?.Value;
+                        if (handle != IntPtr.Zero)
+                        {
+                            MoveSpecificWindow(handle, windowRectangle.Left, windowRectangle.Top, Math.Abs(windowRectangle.Right-windowRectangle.Left),Math.Abs( windowRectangle.Bottom-windowRectangle.Top));
+                        }
+                    }
+                }
+                catch { }
+            }
+
+        }
+
+
+        private void dataGridViewWindows_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridViewWindows.Columns["Button"]?.Index)
+            {
+                var handle = new IntPtr((int)dataGridViewWindows.Rows[e.RowIndex].Cells[1].Value);
+
+                RECT windowRectangle = new RECT();
+                windowRectangle.Left = (int)dataGridViewWindows.Rows[e.RowIndex].Cells["Left"]?.Value;
+                windowRectangle.Top = (int)dataGridViewWindows.Rows[e.RowIndex].Cells["Top"]?.Value;
+                windowRectangle.Right = (int)dataGridViewWindows.Rows[e.RowIndex].Cells["Right"]?.Value;
+                windowRectangle.Bottom = (int)dataGridViewWindows.Rows[e.RowIndex].Cells["Bottom"]?.Value;
+                if (handle != IntPtr.Zero)
+                {
+                    MoveSpecificWindow(handle, 0, 0, Math.Abs (windowRectangle.Right - windowRectangle.Left), Math.Abs (windowRectangle.Bottom - windowRectangle.Top));
+                }
+            }
+
+        }
+
+        private void saveCurrentLayout_Click(object sender, EventArgs e)
+        {
+            GetAllWindows();
+        }
+
+
+
+        private void saveLayoutAsFile()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                object[,] dataGridViewObjectsArray = new object[dataGridViewWindows.Rows.Count, dataGridViewWindows.Columns.Count];
+                for (int x = 0; x < dataGridViewWindows.Rows.Count; x++)
+                    for (int y = 0; y < dataGridViewWindows.Columns.Count; y++)
+                        dataGridViewObjectsArray[x, y] = dataGridViewWindows.Rows[x].Cells[y].Value;
+
+
+                var serializer = new SharpSerializer();
+                serializer.Serialize(dataGridViewObjectsArray, saveFileDialog.FileName);
+            }
+        }
+
+        private void loadLayoutFromFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+
+                var serializer = new SharpSerializer();
+                object[,] dataGridViewObjectsArray = (object[,])serializer.Deserialize(openFileDialog.FileName);
+                dataGridViewWindows.Rows.Clear();
+                for (int x = 0; x < dataGridViewObjectsArray.GetLength(0); x++)
+                {
+                    dataGridViewWindows.Rows.Add();
+                    for (int y = 0; y < dataGridViewObjectsArray.GetLength(1); y++)
+                        dataGridViewWindows.Rows[x].Cells[y].Value = dataGridViewObjectsArray[x, y];
+                }
+            }
+            ResetWindows();
+        }
+
+        private void splitSidewaysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewWindows.SelectedRows.Count;
+            Rectangle rect = Screen.FromHandle(this.Handle).Bounds;
+            int windowWidth = rect.Width / count;
+            for (int i = 0; i < dataGridViewWindows.SelectedRows.Count; i++)
+            {
+                var handle = new IntPtr((int)dataGridViewWindows.SelectedRows[i].Cells[1].Value);
+                //RECT windowRectangle = (RECT)dataGridViewWindows.SelectedRows[i].Cells[3].Value;
+                if (handle != IntPtr.Zero)
+                {
+                    MoveSpecificWindow(handle, i * windowWidth+rect.Left, rect.Top, windowWidth, rect.Bottom);
+                }
+            }
+        }
+
+        private void splitHorizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewWindows.SelectedRows.Count;
+            Rectangle rect = Screen.FromHandle(this.Handle).Bounds;
+            int windowHeight = rect.Height / count;
+            for (int i = 0; i < dataGridViewWindows.SelectedRows.Count; i++)
+            {
+                var handle = new IntPtr((int)dataGridViewWindows.SelectedRows[i].Cells[1].Value);
+                //RECT windowRectangle = (RECT)dataGridViewWindows.SelectedRows[i].Cells[3].Value;
+                if (handle != IntPtr.Zero)
+                {
+                    MoveSpecificWindow(handle, rect.Left,i * windowHeight+rect.Top, rect.Right, windowHeight);
+                }
+            }
+        }
+
+        private void dataGridViewWindows_Click(object sender, EventArgs e)
+        {
+            if (e.GetType() == typeof(DataGridViewCellEventArgs))
+            {
+                dataGridViewWindows_CellContentClick(sender, (DataGridViewCellEventArgs)e);
+            }
+            else if (e.GetType() == typeof(MouseEventArgs))
+            {
+                if (((MouseEventArgs)e).Button == MouseButtons.Right)
+                {
+                    contextMenuStrip1.Show(Cursor.Position.X, Cursor.Position.Y);
+                }
+
+
+            }
+        }
+
+        private void loadLayoutFromFileButton_Click(object sender, EventArgs e)
+        {
+            loadLayoutFromFile();
+        }
+
+        private void saveLayoutAsFileButton_Click(object sender, EventArgs e)
+        {
+            saveLayoutAsFile();
+        }
+
+        private void loadLastLayout_Click(object sender, EventArgs e)
+        {
+            ResetWindows();
+        }
+
+        private void minimizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewWindows.SelectedRows.Count;
+            for (int i = 0; i < dataGridViewWindows.SelectedRows.Count; i++)
+            {
+                var handle = new IntPtr((int)dataGridViewWindows.SelectedRows[i].Cells[1].Value);
+                if (handle != IntPtr.Zero)
+                {
+                    ShowWindow(handle, SW_MINIMIZE);
+
+                }
+            }
+        }
+
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewWindows.SelectedRows.Count;
+            for (int i = 0; i < dataGridViewWindows.SelectedRows.Count; i++)
+            {
+                var handle = new IntPtr((int)dataGridViewWindows.SelectedRows[i].Cells[1].Value);
+                if (handle != IntPtr.Zero)
+                {
+                    BringtoFront(handle);
+
+                }
+            }
+        }
+
+        private void BringtoFront(IntPtr handle)
+        {
+            ShowWindow(handle, SW_RESTORE);
+        }
+    }
+}
